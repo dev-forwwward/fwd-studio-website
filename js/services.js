a2f1256c73f920d07d6ff57b22d57b65290a7369
beatriz-araujo <beatriz@forwwward.studio>
2026-07-06 20:33:18 +0100

    Services info fade-in/out update
    Now includes fade-in/out of info blocks

--- js/services.js
+++ js/services.js
@@ -6,32 +6,47 @@ export function servicesInit() {
         const fixedContentList = document.querySelectorAll(".services-list-item-fixed-content-container");
 
         servicesListItems.forEach((item, i) => {
-            ScrollTrigger.create({
-                trigger: item,
-                start: "top 70%",
-                end: "bottom top",
-                scrub: true,
-                markers: true,
-                onEnter: () => {
-                    const prevActive = servicesList.querySelector('.active');
-                    if (prevActive) {
-                        prevActive.classList.remove("active");
+
+            const textBlock = fixedContentList[i].querySelector(".services-list-item-fixed-content_text_block");
+
+            gsap.timeline({
+                scrollTrigger: {
+                    trigger: item,
+                    start: "top 70%",
+                    end: "bottom top",
+                    scrub: true,
+                    markers: true,
+                    onEnter: () => {
+                        const prevActive = servicesList.querySelector('.active');
+                        if (prevActive && i != 0) {
+                            prevActive.classList.remove("active");
+                        }
+                        fixedContentList[i].classList.add("active");
+                    },
+                    onLeave: () => {
+                        fixedContentList[i].classList.remove("active");
+                    },
+                    onEnterBack: () => {
+                        const prevActive = servicesList.querySelector('.active');
+                        if (prevActive) {
+                            prevActive.classList.remove("active");
+                        }
+                        fixedContentList[i].classList.add("active");
+                    },
+                    onLeaveBack: () => {
+                        if (i != 0) {
+                            fixedContentList[i].classList.remove("active");
+                        }
                     }
-                    fixedContentList[i].classList.add("active");
-                },
-                onLeave: () => {
-                    fixedContentList[i].classList.remove("active");
-                },
-                onEnterBack: () => {
-                    const prevActive = servicesList.querySelector('.active');
-                    if (prevActive) {
-                        prevActive.classList.remove("active");
-                    }
-                    fixedContentList[i].classList.add("active");
-                },
-                onLeaveBack: () => {
-                    fixedContentList[i].classList.remove("active");
                 }
+            }).from(textBlock, {
+                opacity: 0,
+                duration: .25,
+            }).from({}, {
+                duration: .65
+            }).to(textBlock, {
+                opacity: 0,
+                duration: .1,
             });
         });
     }
